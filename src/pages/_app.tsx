936549ef24328c02b8ad5c69176d858fd09b1225
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-03-18 17:34:04 +0300

    feat(utils): Add fetcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,22 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Layout } from "@/components/Layout";
-import "@/styles/global.css"
+import { ToastContainer } from "react-toastify";
+import "@/styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function App({Component, pageProps}: AppProps) {
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Watch With Friends</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-
       <Layout>
-        <Component
-          {...pageProps} />
+        <Component {...pageProps} />
+        <ToastContainer theme={"dark"} autoClose={1500} limit={2} />
       </Layout>
-
     </>
-  )
+  );
 }
---
+++ src/shared/lib/utils/fetcher.ts
@@ -0,0 +1,13 @@
+import { toast } from "react-toastify";
+
+export const fetcher = async <T = unknown>(url: string, body?: RequestInit, customMessage?: string): Promise<T | null> => {
+  const response = await fetch(url, body);
+  const data = await response.json();
+
+  if (response.ok) {
+    return data;
+  } else {
+    toast.warning((customMessage || "An error occurred while getting data. ") + `Code: ${data.code}`);
+    return null;
+  }
+};
